4793be8949acc1ef1bc405d62ccbe0b8115cb04a
Paul McQuade <paulmcquad@gmail.com>
2016-11-14 20:07:43 +0000

    Finished Lesson 2 Components - C Code
    Finished Chapter 2 and examples
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ CODE/C/WEEK 2 - Lesson 2 Components/ex02-04.c
@@ -0,0 +1,14 @@
+/* ex03-04.c */
+#include <stdio.h>
+
+int main(void)
+{
+    int ctr;
+
+    for( ctr = 65; ctr < 91; ctr++ )
+        printf("%c", ctr );
+
+    return 0;
+}
+/* end of program */
+
---
+++ CODE/C/WEEK 2 - Lesson 2 Components/ex02-05.c
@@ -0,0 +1,16 @@
+/* ex02-05.c */
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+    char buffer[256];
+
+    printf( "Enter your name and press <Enter>:\n");
+    gets( buffer );
+
+     printf( "\nYour name has %d characters and spaces!",
+                      strlen( buffer ));
+
+    return 0;
+}
+
